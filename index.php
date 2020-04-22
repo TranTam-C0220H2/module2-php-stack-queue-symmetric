bbdcae6e1505b15b5f9b0019f310becbd5a1d7c5
TranTam-C0220H2 <tam.tvt.ptit@gmail.com>
2020-04-22 20:56:45 +0700

    up review

--- index.php
+++ index.php
@@ -20,27 +20,31 @@ include 'Queue.php';
 $queue = new Queue();
 $stack = new Stack(100);
 $string = $_POST['string'];
-try {
-    for ($i = 0; $i < strlen($string); $i++) {
-        $queue->enqueue($string[$i]);
-        $stack->push($string[$i]);
-    }
-    $count = 0;
-    $halfString = floor(strlen($string) / 2);
-    for ($i = 0; $i < $halfString; $i++) {
-        if ($queue->dequeue() != $stack->pop()) {
-            break;
-        } else {
-            $count++;
+if ($string == '') {
+    echo 'Yeu cau nhap chuoi can kiem tra';
+} else {
+    try {
+        for ($i = 0; $i < strlen($string); $i++) {
+            $queue->enqueue($string[$i]);
+            $stack->push($string[$i]);
         }
+        $count = 0;
+        $halfString = floor(strlen($string) / 2);
+        for ($i = 0; $i < $halfString; $i++) {
+            if ($queue->dequeue() != $stack->pop()) {
+                break;
+            } else {
+                $count++;
+            }
+        }
+        if ($count == $halfString) {
+            echo "String is symmetric.";
+        } else {
+            echo "String isn't symmetric.";
+        }
+    } catch (Exception $exception) {
+        echo $exception->getMessage();
     }
-    if ($count == $halfString) {
-        echo "String is symmetric.";
-    } else {
-        echo "String isn't symmetric.";
-    }
-} catch (Exception $exception) {
-    echo $exception->getMessage();
 }
 ?>
 </body>
